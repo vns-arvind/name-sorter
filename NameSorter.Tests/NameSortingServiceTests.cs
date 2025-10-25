1ebf0f3608c2154d5d912158c06cbad0808eb914
Arvind Pandey <arvindpandeyvns@gmail.com>
2025-10-25 15:55:15 +0100

    test: name sorting service (red)

---
+++ NameSorter.Tests/NameSortingServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using NameSorter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSorter.Tests
+{
+    public class NameSortingServiceTests
+    {
+        [Test]
+        public void NameSortingService_SortsByLastName()
+        {
+            var service = new NameSortingService();
+            var names = new[] { new Person("Jane Doe"), new Person("John Smith") };
+            var sorted = service.SortNames(names).ToList();
+            Assert.That("Jane Doe", sorted[0].ToString());
+            Assert.That("John Smith", sorted[1].ToString());
+        }
+    }
+}
